5de0cc937750bbda4280fbbce2a71d2ebb80d4d7
Marcelo Victor <mvbs3@cin.ufpe.br>
2023-07-20 23:33:52 -0300

    adding deploy cards + footer description

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,20 +12,30 @@ function App() {
     const core = {
       Oai5gSa: {
         Title: "OpenAir 5g SA",
-        Description: "lore ipsum",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum risus nec libero faucibus faucibus.",
+        Img: "openAir5gLogo.png",
+        Url: "inserir diretorio pra acessar a pagcerta",
+      },
+      Oai4g: {
+        Title: "OpenAir 4g SA",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum risus nec libero faucibus faucibus.",
         Img: "openAir5gLogo.png",
         Url: "inserir diretorio pra acessar a pagcerta",
       },
       Open5gSa: {
         Title: "Open5gs 5g SA",
-        Description: "lore ipsum",
-        Img: "contemplative-reptile.jpg",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum risus nec libero faucibus faucibus.",
+        Img: "openAir5gLogo.png",
         Url: "inserir diretorio pra acessar a pagcerta",
       },
       Open5gNsa: {
         Title: "Open5gs 5g NSA",
-        Description: "lore ipsum",
-        Img: "contemplative-reptile.jpg",
+        Description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum risus nec libero faucibus faucibus.",
+        Img: "openAir5gLogo.png",
         Url: "inserir diretorio pra acessar a pagcerta",
       },
     };
@@ -38,7 +48,6 @@ function App() {
             Description={core[key].Description}
             Img={core[key].Img}
             Url={core[key].Url}
-            style={{ padding: 10 }}
           />
         </div>
       );
@@ -94,9 +103,52 @@ function App() {
           </div>
         </div>*/}
       </Container>
-      <Box sx={{ bgcolor: "#cfe8fc", height: "50vh" }}>
-        <Container sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Box
+        sx={{
+          bgcolor: "#1976D2",
+          height: "50vh",
+          alignItems: "center",
+          flexDirection: "columns",
+        }}
+      >
+        <h1
+          style={{
+            justifyContent: "center",
+            textAlign: "center",
+            color: "black",
+            display: "block",
+            padding: 5,
+          }}
+        >
+          Deploys
+        </h1>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           {cardsEnviroment()}
+        </Container>
+      </Box>
+      <Box>
+        <Container>
+          <h1 style={{ textAlign: "center" }}>Sobre o laboratório</h1>
+          <h2 style={{ textAlign: "center" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            elementum risus nec libero faucibus faucibus. Suspendisse potenti.
+            Nullam vulputate non sapien ac suscipit. Cras ac nisi mattis,
+            tincidunt nibh vel, sagittis massa. Cras scelerisque augue in
+            elementum bibendum. Morbi quam tortor, malesuada nec urna et,
+            molestie scelerisque sapien. Suspendisse pulvinar efficitur arcu, ut
+            rutrum est porta id. Morbi dolor lectus, commodo at fermentum at,
+            laoreet vitae nibh. Maecenas at nulla elementum, lobortis eros in,
+            luctus ligula. Cras pulvinar elit sed magna rhoncus, eu viverra nibh
+            placerat. Proin at posuere quam. Phasellus tempus at metus vel
+            finibus. Nunc lobortis bibendum ante lacinia congue. Duis ut gravida
+            odio.
+          </h2>
         </Container>
       </Box>
     </React.Fragment>
--- front-end/src/Components/Header.js
+++ front-end/src/Components/Header.js
@@ -13,7 +13,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import lab5gLogo from "./img/logo.png";
 import DeployButton from "./DeployButton";
-const pages = ["Products", "Pricing", "Blog"];
+const pages = ["Home", "Projects", "Contacts"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Header() {
